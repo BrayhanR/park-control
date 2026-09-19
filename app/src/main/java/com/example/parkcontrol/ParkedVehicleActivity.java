8a24dbc2053cb1ca3625e0a4dbc4670c4cf8c048
Brayhan <brayhanstein96@gmail.com>
2026-09-19 00:45:04 -0500

    Cambios de prueba

--- app/src/main/java/com/example/parkcontrol/ParkedVehicleActivity.java
+++ app/src/main/java/com/example/parkcontrol/ParkedVehicleActivity.java
@@ -20,5 +20,7 @@ public class ParkedVehicleActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        // Cambios de prueba
     }
 }
